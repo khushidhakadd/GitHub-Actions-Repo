f4ef65e0da42614333bb8a439d1e521fe69817d0
khushidhakadd <84145722+khushidhakadd@users.noreply.github.com>
2024-01-30 06:21:59 +0000

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -13,9 +13,9 @@ provider "azurerm" {
   features {
   }
   
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
+  client_id       = "995bfbc1-3c1a-447e-a24d-8ddeb69fb2ae"
+  client_secret   = "ZlE8Q~UlkTIXjDLnfwhWk-0fdsMKxjyyVt6qXa8G"
+  tenant_id       = "b1b6657a-30c8-4e82-9f30-bf5a272aa1c2"
+  subscription_id = "959c9715-ad9d-4e9f-9cad-897421593049"
   skip_provider_registration = true
 }
