0e4d036dcf73207e4c530ff9313b48bf18b16f25
khushidhakadd <84145722+khushidhakadd@users.noreply.github.com>
2024-01-30 06:21:01 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -9,19 +9,3 @@ variable "resource_group_name" {
   default     = "rg-1211"
   description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
-
-variable "client_id" {
-   type        = string
-}
-
-variable "client_secret" {
-   type        = string
-}
-
-variable "tenant_id" {
-   type        = string
-}
-
-variable "subscription_id" {
-   type        = string
-}
